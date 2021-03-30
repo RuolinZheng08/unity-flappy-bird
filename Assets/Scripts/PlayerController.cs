9db7f61bd8afe369071631c3db90b947dcdd0749
Lynn Zheng <ruolinzheng@uchicago.edu>
2021-03-29 20:24:31 -0700

    Implement basic player control

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float yBoundary = 8;
+    [SerializeField] float upForce;
+    Rigidbody playerRb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // keep player from flying too high
+        if (transform.position.y > yBoundary) {
+            transform.position = new Vector3(transform.position.x, yBoundary, transform.position.z);
+            // clear force
+            playerRb.velocity = Vector3.zero;
+            playerRb.angularVelocity = Vector3.zero;
+        }
+        if (Input.GetKey(KeyCode.Space)) {
+            playerRb.AddForce(Vector3.up * upForce);
+        }
+    }
+}

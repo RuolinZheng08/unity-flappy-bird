49ebc5c6b44c8b209d6a12c019b30b6ba6ae7cc9
Lynn Zheng <ruolinzheng@uchicago.edu>
2021-03-30 19:51:57 -0700

    Make background repeating

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,13 +7,12 @@ public class PlayerController : MonoBehaviour
     float yBoundary = 8;
     [SerializeField] float upForce;
     Rigidbody playerRb;
-    // Start is called before the first frame update
+
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         // keep player from flying too high
@@ -21,7 +20,6 @@ public class PlayerController : MonoBehaviour
             transform.position = new Vector3(transform.position.x, yBoundary, transform.position.z);
             // clear force
             playerRb.velocity = Vector3.zero;
-            playerRb.angularVelocity = Vector3.zero;
         }
         if (Input.GetKey(KeyCode.Space)) {
             playerRb.AddForce(Vector3.up * upForce);
